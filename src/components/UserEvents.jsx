8c69c9d054bbb8ec5f0d0e501312437fce45aec5
Mahmoud Sayed <mahmoudsayed1612@gmail.com>
2025-05-18 18:23:30 +0300

    Fix import path for useEvents hook in UserEvents and Home components

--- src/components/UserEvents.jsx
+++ src/components/UserEvents.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useEvents } from "../hocks/useEvent.js";
+import { useEvents } from "../hooks/useEvent.js";
 import {
     Card,
     CardContent,
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useEvents } from "../hocks/useEvent.js";
+import { useEvents } from "../hooks/useEvent.js";
 import {
     Box,
     Typography,
